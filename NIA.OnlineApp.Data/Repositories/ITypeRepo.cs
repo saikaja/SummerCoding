822200b756d7f57f6bc67d7adb94e859e4e7df39
vekaria04 <yvekaria@uwo.ca>
2025-05-07 09:35:12 -0400

    Repo

---
+++ NIA.OnlineApp.Data/Repositories/ITypeRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using NIA.OnlineApp.Data.Entities;
+
+namespace NIA.OnlineApp.Data.Repositories
+{
+    public interface ITypeRepo
+    {
+        Task<IEnumerable<TypeInformation>> GetByTypeIdAsync(int typeId);
+        Task AddRangeAsync(IEnumerable<TypeInformation> entries);
+        Task UpdateAsync(TypeInformation entry);
+        Task DeleteAsync(int id);
+    }
+
+
+}
--- NIA.OnlineApp.Data/Repositories/TypeRepo.cs
+++ NIA.OnlineApp.Data/Repositories/TypeRepo.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NIA.OnlineApp.Data.Entities;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NIA.OnlineApp.Data.Repositories
 {
-    internal class TypeRepo
+    public class TypeRepo : ITypeRepo
     {
+        private readonly AppDbContext _context;
+
+        public TypeRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TypeInformation>> GetByTypeIdAsync(int typeId)
+        {
+            return await _context.TypeInformations
+                                 .Where(ti => ti.Type_Id == typeId)
+                                 .ToListAsync();
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TypeInformation> entries)
+        {
+            await _context.TypeInformations.AddRangeAsync(entries);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TypeInformation entry)
+        {
+            _context.TypeInformations.Update(entry);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.TypeInformations.FindAsync(id);
+            if (entity != null)
+            {
+                _context.TypeInformations.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
