2226d534833b3a0487f4cb1fb68380c9ecfa93c2
Sai Kaja <saikaja99@gmail.com>
2025-05-07 12:51:43 -0400

    added service layer

---
+++ NIA.OnlineApp.InteractiveAPI/Services/ITypeInformationService.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using NIA.OnlineApp.Data.Entities;
+
+namespace NIA.OnlineApp.InteractiveAPI.Services
+{
+    public interface ITypeInformationService
+    {
+        IEnumerable<TypeInformation> GetAllAttributes();
+        bool InsertAttributes(TypeInformation typeInformation);
+        bool UpdateAttributes(int Type_Id, TypeInformation typeInformation);
+
+        bool DeleteAttributes(int Type_Id, TypeInformation typeInformation);
+    }
+}
---
+++ NIA.OnlineApp.InteractiveAPI/Services/ITypeUtilService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Win32;
+using NIA.OnlineApp.Data.Entities;
+
+namespace NIA.OnlineApp.InteractiveAPI.Services
+{
+    public interface ITypeUtilService
+    {
+        IEnumerable<TypeUtil> GetAllEvents();
+        bool InsertEvent(TypeUtil typeUtil);
+        bool UpdateEvent(int Id, TypeUtil typeUtil);
+
+        bool DeleteAttributes(int Id, TypeUtil typeUtil);
+    }
+}
