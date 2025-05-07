5911f2fa5df8e0de22a749d10ade0500292da1e1
Sai Kaja <saikaja99@gmail.com>
2025-05-07 09:52:39 -0400

    added negative testing to typerepo

--- NIA.OnlineApp.Data/Repositories/ITypeRepo.cs
+++ NIA.OnlineApp.Data/Repositories/ITypeRepo.cs
@@ -12,8 +12,8 @@ namespace NIA.OnlineApp.Data.Repositories
     {
         Task<IEnumerable<TypeInformation>> GetByTypeIdAsync(int typeId);
         Task AddRangeAsync(IEnumerable<TypeInformation> entries);
-        Task UpdateAsync(TypeInformation entry);
-        Task DeleteAsync(int id);
+        Task UpdateAsync(int id, TypeInformation entry);
+        Task DeleteAsync(int id, TypeInformation entry);
     }
 
 
