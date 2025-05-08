1d54b38f7b2fae64fca79668c5f56ddb515ebaff
vekaria04 <yvekaria@uwo.ca>
2025-05-08 11:03:57 -0400

    business data model

---
+++ NIA.OnlineApp.Data/Entities/BusinessData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIA.OnlineApp.Data.Entities
+{
+    public class BusinessData
+    {
+        public int Id { get; set; }  // stays in DB, not exposed in API
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+
+}
