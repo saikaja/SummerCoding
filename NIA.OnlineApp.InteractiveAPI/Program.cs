ef99544206df086ab57b5cb0092fea5bb4899448
vekaria04 <yvekaria@uwo.ca>
2025-05-08 10:37:15 -0400

    ignore nulls

--- NIA.OnlineApp.InteractiveAPI/Program.cs
+++ NIA.OnlineApp.InteractiveAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
@@ -22,6 +23,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     
 }
 );
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
 
 builder.Services.AddScoped<ITypeUtilRepo, TypeUtilRepo>();
 builder.Services.AddScoped<ITypeInformationRepo, TypeInformationRepo>();
