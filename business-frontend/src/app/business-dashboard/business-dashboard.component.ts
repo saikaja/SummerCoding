0c613b94d8ab435c2c87dc8c9b010550db1e5042
vekaria04 <yvekaria@uwo.ca>
2025-05-08 21:31:42 -0400

    add angular frontend

---
+++ business-frontend/src/app/business-dashboard/business-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './business-dashboard.component.html',
+  styleUrl: './business-dashboard.component.css'
+})
+export class BusinessDashboardComponent {
+
+}
