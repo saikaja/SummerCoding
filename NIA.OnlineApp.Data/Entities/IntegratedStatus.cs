a5ea4eac228f3235863d912190236bb2b709419e
Sai Kaja <saikaja99@gmail.com>
2025-05-10 10:45:14 -0400

    added integrated tables

---
+++ NIA.OnlineApp.Data/Entities/IntegratedStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY.OnlineApp.Data.Entities
+{
+    internal class IntegratedStatus
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("TypeUtil")]
+        public required int Integrated_Id { get; set; }
+        public required bool IsDataIntegrated { get; set; }
+        public IntegratedType? IntegratedType { get; set; } = null!;
+    }
+}
---
+++ NIA.OnlineApp.Data/Entities/IntegratedType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY.OnlineApp.Data.Entities
+{
+    public class IntegratedType
+    {
+        public int Id { get; set; }
+        public required string Type { get; set; }
+        public required string Description { get; set; }
+    }
+}
