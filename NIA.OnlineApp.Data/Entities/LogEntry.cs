e235727237b2ea0976dc908e6a34ec46461514ac
Sai Kaja <saikaja99@gmail.com>
2025-05-14 11:10:26 -0400

    created logentry

---
+++ NIA.OnlineApp.Data/Entities/LogEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY.OnlineApp.Data.Entities
+{
+    internal class LogEntry
+    {
+        public int Id { get; set; }
+        public string StackTrace { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public string Source { get; set; } = null;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
